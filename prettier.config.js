6186398ae66517b68b63280aa90492642e4eaaf9
Yuliya Zabolotnayae <yuv.zabolotnaya@gmail.com>
2022-06-09 21:03:07 +0300

    feat: add file structure and html file content

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
